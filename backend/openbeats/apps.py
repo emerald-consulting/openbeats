527c533d6cf22d246ef629d720cf634d55a294bb
RyChrome <dilsy3@gmail.com>
2021-09-14 09:11:37 -0400

    start app openbeats in backend root directory. Allows us to create tests and models

---
+++ backend/openbeats/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OpenbeatsConfig(AppConfig):
+    name = 'openbeats'
---
+++ backend/openbeats/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+class User(Models.model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)

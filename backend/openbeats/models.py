00057ddd2ad11e0dc9d98fccbbae2bda9d394c03
RyChrome <dilsy3@gmail.com>
2021-09-25 09:55:43 -0400

    #20 User model

--- backend/openbeats/models.py
+++ backend/openbeats/models.py
@@ -1,3 +1,9 @@
 from django.contrib.auth.models import User
 
 # Create your models here.
+class User(Models.model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    

cf3b625c3c02138cbd673a8d2ba3855025f3d8a8
MIchael Focacci <michaelfocacci@gmail.com>
2021-09-25 21:52:13 -0400

    Replace User with CustomUser

--- backend/openbeats/serializers.py
+++ backend/openbeats/serializers.py
@@ -1,15 +1,15 @@
 from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
-from openbeats.models import User
+from openbeats.models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = CustomUser
         fields = ['id', 'first_name', 'last_name', 'username', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        user = User(
+        user = CustomUser(
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
             username=validated_data['username']

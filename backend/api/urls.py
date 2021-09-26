9da83d97040a750a4532109103e2475f63dac8f6
MIchael Focacci <michaelfocacci@gmail.com>
2021-09-26 17:50:38 -0400

    Moved jwt auth url's from openbeats to api
    This solved my rest_framework_simplejwt import error

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+        # JWT paths
+    path('token/obtain/',
+         jwt_views.TokenObtainPairView.as_view(),
+         name='token_create'),
+
+    path('token/refresh/',
+         jwt_views.TokenRefreshView.as_view(),
+         name='token_refresh'),
+
+    # Keep on bottom
     path('', include('openbeats.urls'))
 ]
--- backend/openbeats/urls.py
+++ backend/openbeats/urls.py
@@ -1,19 +1,9 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from rest_framework_simplejwt import views as jwt_views
 from openbeats import views
 
 urlpatterns = [
     path('api/register/', views.Register.as_view()),
-
-    # JWT paths
-    path('token/obtain/',
-         jwt_views.TokenObtainPairView.as_view(),
-         name='token_create'),
-
-    path('token/refresh/',
-         jwt_views.TokenRefreshView.as_view(),
-         name='token_refresh')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

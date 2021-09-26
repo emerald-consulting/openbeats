4fd4df4a68ec3f8cfb783df0f4368f0eab2b2f97
MIchael Focacci <michaelfocacci@gmail.com>
2021-09-25 20:45:51 -0400

    AUTH_USER_MODEL
    This will have to be set to the user model we create eventually

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -156,3 +156,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Set equal to user model we use in openbeats/models.py
+#AUTH_USER_MODEL = _____

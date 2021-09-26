5fe23ef276b9fb5b2b318e33bb3b8378c2ef5b0b
MIchael Focacci <michaelfocacci@gmail.com>
2021-09-25 20:49:49 -0400

    Some information about User model

--- backend/openbeats/models.py
+++ backend/openbeats/models.py
@@ -1,1 +1,17 @@
 from django.contrib.auth.models import User
+
+'''
+User model comes with these fields built in:
+
+username (required)
+password (required)
+first_name
+last_name
+email
+user_permissions
+is_staff
+is_active
+is_superuser
+last_login
+date_joined
+'''

5b522f2b7a399c055bb0d762c19842f07367b963
Jordan Bailey <bailey8@buffalo.edu>
2021-09-27 03:22:24 -0400

    Email and Password auth and JWT setup #45

---
+++ backend/authentication/views.py
@@ -0,0 +1,49 @@
+from django.contrib.auth import authenticate
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import MyTokenObtainPairSerializer, CustomUserSerializer
+
+
+class ObtainTokenPairWithColorView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class CustomUserCreate(APIView):
+
+    # We need to specify an empty list or tuple for authentication_classes in addition 
+    # to setting permission_classes to convince DRF to open up a view to the public.
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
+
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class isLoggedIn(APIView):
+
+    def get(self, request):
+        return Response(data="User logged in", status=status.HTTP_200_OK)
+
+class LogoutAndBlacklistRefreshTokenForUserView(APIView):
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            # token.blacklist() - this causes a 400 error - why?
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)

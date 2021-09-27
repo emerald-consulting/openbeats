d2d8a3e886f6f1c1d5135b5655a5fe5659f00c1f
Kyle Alberry <kylealberry@gmail.com>
2021-09-27 18:25:18 -0400

    setup database configs

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -77,13 +77,6 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-        'default': {
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': os.getenv('DATABASE_URL'),
-        'PORT': 5432,
-    }
 }
 
 
@@ -129,3 +122,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)

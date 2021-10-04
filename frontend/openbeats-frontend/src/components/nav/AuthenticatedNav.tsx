f6e242f4638bc5cdb986668a8c839ca1d9505566
RyChrome <dilsy3@gmail.com>
2021-10-04 12:31:17 -0400

    Triple equal sign instead of double equal sign.

--- frontend/openbeats-frontend/src/components/nav/AuthenticatedNav.tsx
+++ frontend/openbeats-frontend/src/components/nav/AuthenticatedNav.tsx
@@ -97,7 +97,7 @@ export default function Example() {
                       <a
                         onClick={item.onclick}
                         className={
-                          item.pathname == location.pathname ? 'cursor-pointer border-green2 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium' : 'cursor-pointer border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
+                          item.pathname === location.pathname ? 'cursor-pointer border-green2 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium' : 'cursor-pointer border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium'
                         }
                       >
                         {item.name}
@@ -211,7 +211,7 @@ export default function Example() {
                   <a
                     onClick={item.onclick}
                     className={
-                      item.pathname == location.pathname ? 'cursor-pointer border-green1 text-green2 block pl-3 pr-4 py-2 border-l-4 text-base font-medium' : 'cursor-pointer border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
+                      item.pathname =-= location.pathname ? 'cursor-pointer border-green1 text-green2 block pl-3 pr-4 py-2 border-l-4 text-base font-medium' : 'cursor-pointer border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 block pl-3 pr-4 py-2 border-l-4 text-base font-medium'
                     }
                   >
                     {item.name}

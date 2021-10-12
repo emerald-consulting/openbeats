8d1c1845888096cb74b4463c8a2be996ca8c73e4
Kyle Alberry <kylealberry@gmail.com>
2021-10-12 15:01:03 -0400

    updated static root folder

--- backend/openbeats_backend/settings.py
+++ backend/openbeats_backend/settings.py
@@ -163,7 +163,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'build')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

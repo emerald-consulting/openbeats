d207a299765b140f6455c9eb72537e1674db12c0
MIchael Focacci <michaelfocacci@gmail.com>
2021-10-16 16:44:14 -0400

    Nested spotifyToken in User model

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from spotify.models import SpotifyToken
 
 # https://stackoverflow.com/questions/18853688/django-1-6-abstractuser-not-working
 class CustomUser(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    spotifyToken = models.ForeignKey(to=SpotifyToken, on_delete=models.CASCADE, default=None, null=True)
 
 CustomUser._meta.get_field('email')._unique=True
 

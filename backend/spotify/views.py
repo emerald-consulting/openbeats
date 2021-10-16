fa9cfbb73fb12d9a9eae1ffbf8bcf3adf208aee3
MIchael Focacci <michaelfocacci@gmail.com>
2021-10-16 14:39:27 -0400

    Created view for spotify authorization and retrieve tokens

--- backend/spotify/views.py
+++ backend/spotify/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from .credentials import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from requests import Request, post
 
-# Create your views here.
+class AuthURL(APIView):
+
+    def get(self, request, format=None):
+        scopes = 'user-read-email'
+
+        url = Request('GET', 'https://accounts.spotify.com/authorize', params={
+            'scope': scopes,
+            'response_type': 'code',
+            'redirect_uri': REDIRECT_URI,
+            'client_id': CLIENT_ID
+        }).prepare().url
+
+        return Response({'url': url}, status=status.HTTP_200_OK)
+
+
+def spotify_callback(request, format=None):
+    code = request.GET.get('code')
+    error = request.GET.get('error')
+
+    response = post('https://accounts.spotify.com/api/token', data={
+        'grant_type': 'autohorization_code',
+        'code': code,
+        'redirect_uri': REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }).json()
+
+    access_token = response.get('access_token')
+    token_type = response.get('token_type')
+    refresh_token = response.get('refresh_token')
+    expires_in = response.get('expires_in')
+    error = response.get('error')

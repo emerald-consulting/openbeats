6a87342363e1eb3879a15a8c0c8c33e54f0745e2
MIchael Focacci <michaelfocacci@gmail.com>
2021-10-16 14:40:33 -0400

    Included spotify.urls in root urlpatterns

--- backend/openbeats_backend/urls.py
+++ backend/openbeats_backend/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls'))
-
+    path('api/', include('authentication.urls')),
+    path('spotify/', include('spotify.urls'))
 ]

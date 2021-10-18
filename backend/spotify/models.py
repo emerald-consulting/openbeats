df7bc38a6ead45df07cc43689306e1b0fcd37b69
MIchael Focacci <michaelfocacci@gmail.com>
2021-10-18 19:03:40 -0400

    Changed user key to session_id

--- backend/spotify/models.py
+++ backend/spotify/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from authentication.models import CustomUser
 
 class SpotifyToken(models.Model):
-    user = models.CharField(max_length=50, unique=True)
+    session_id = models.CharField(max_length=50, unique=True, default="session_id")
     created_at = models.DateTimeField(auto_now_add=True)
     refresh_token = models.CharField(max_length=150)
     access_token = models.CharField(max_length=150)
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length=50)
+    #user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=None)

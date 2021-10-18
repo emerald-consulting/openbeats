310fbf3f1761ff431bf6764d7d442ee46c21c3e4
MIchael Focacci <michaelfocacci@gmail.com>
2021-10-18 19:04:06 -0400

    Updated spotify urls

--- backend/spotify/urls.py
+++ backend/spotify/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path 
-from .views import AuthURL, spotify_callback
+from .views import SpotifyAuthURL, SpotifyEmailURL, spotify_callback
+
+app_name = 'spotify'
 
 urlpatterns = [
-    path('get-auth-url', AuthURL.as_view()),
-    path('redirect', spotify_callback)
+    path('get-auth-url', SpotifyAuthURL.as_view()),
+    path('redirect', spotify_callback),
+    path('get-email', SpotifyEmailURL.as_view(), name='get-email')
 ]

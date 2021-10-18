74fac1745b8872694b42c84b67da7b505d3d7be2
RyChrome <dilsy3@gmail.com>
2021-10-18 10:21:25 -0400

    whoops

--- frontend/src/components/landing/landing/LandingAbout.tsx
+++ frontend/src/components/landing/landing/LandingAbout.tsx
@@ -31,16 +31,6 @@ export default function LandingAbout() {
   return (
     <div className="py-12 bg-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="lg:text-center">
-          <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            Collab, Produce, Share
-          </p>
-          <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in
-            accusamus quisquam.
-          </p>
-        </div>
-
         <div className="mt-10">
         </div>
       </div>

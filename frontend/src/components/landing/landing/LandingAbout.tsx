3a4a352371e08fa7de0ae079028c6433e46c662a
RyChrome <dilsy3@gmail.com>
2021-10-19 21:49:48 -0400

    Updated frontend images and text

--- frontend/src/components/landing/landing/LandingAbout.tsx
+++ frontend/src/components/landing/landing/LandingAbout.tsx
@@ -34,11 +34,5 @@ const features = [
 ];
 
 export default function LandingAbout() {
-  return (
-    <div className="py-12 bg-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="mt-10" />
-      </div>
-    </div>
-  );
+  return <div className="py-12 bg-white" />;
 }

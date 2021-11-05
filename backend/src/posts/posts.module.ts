c776b951b2cb06eea5fe31ee55497f0c747bd569
MIchael Focacci <michaelfocacci@gmail.com>
2021-11-05 18:46:59 -0400

    Add FilesService to posts module

--- backend/src/posts/posts.module.ts
+++ backend/src/posts/posts.module.ts
@@ -3,10 +3,11 @@ import PostsController from './posts.controller';
 import PostsService from './posts.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import Post from './entities/post.entity';
+import { FilesService } from 'src/files/files.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostsController],
-  providers: [PostsService],
+  providers: [PostsService, FilesService],
 })
 export class PostsModule {}

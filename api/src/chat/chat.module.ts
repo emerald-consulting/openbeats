cdb192d514a09c020f46bdb497eb4b6ef17ea909
RyChrome <ryandils@buffalo.edu>
2021-11-05 12:45:19 -0400

    whoops

--- api/src/chat/chat.module.ts
+++ api/src/chat/chat.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ChatService } from './chat.service';
 import { ChatGateway } from './chat.gateway';
+import { ChatService } from './chat.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import Message from './message.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  providers: [ChatGateway, ChatService]
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([Message]),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class ChatModule {}

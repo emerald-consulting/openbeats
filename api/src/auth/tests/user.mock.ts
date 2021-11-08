cf1e4f6e85aa70c46f6d99a658de2f3f88a209d7
RyChrome <ryandils@buffalo.edu>
2021-11-08 14:00:40 -0500

    Updated mocked user model

--- api/src/auth/tests/user.mock.ts
+++ api/src/auth/tests/user.mock.ts
@@ -2,9 +2,6 @@ import { User } from '../../users/entities/user.entity';
 
 const mockedUser: User = {
   id: 1,
-  firstName: 'Ryan',
-  lastName: 'Dils',
-  username: 'RyChrome',
   email: 'ryandils@buffalo.edu',
   password: 'hash',
   isAuthenticated: true,

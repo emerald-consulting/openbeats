8a4acaa6193030bacd4ae4b73c257cb6e39965c0
MIchael Focacci <michaelfocacci@gmail.com>
2021-11-19 12:55:20 -0500

    New Post button says new post

--- web/pages/feed/index.tsx
+++ web/pages/feed/index.tsx
@@ -212,9 +212,7 @@ export default function Feed() {
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
                 <h1 className="text-2xl font-semibold text-gray-900">Feed</h1>
                 <br/>
-                <br/>
-                props.render&&
-                <button type="submit" onClick={handleTextArea}>Preview</button>
+                <button type="submit" onClick={handleTextArea}>New Post</button>
                 {isTextAreaShown && <TextArea/>}
               </div>
               <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">

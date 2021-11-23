8f648f2f2dffec88d5c284b81f196a5e849ac863
RyChrome <ryandils@buffalo.edu>
2021-11-23 13:16:53 -0500

    made download link name of file

--- web/pages/feed/Card.tsx
+++ web/pages/feed/Card.tsx
@@ -30,7 +30,7 @@ const people = [
           const url = window.URL.createObjectURL(new Blob([response.data]));
           const link = document.createElement('a');
           link.href = url;
-          link.setAttribute('download', 'file.wav'); //or any other extension
+          link.setAttribute('download', `${post.title}.wav`); //or any other extension
           document.body.appendChild(link);
           link.click();
         });
